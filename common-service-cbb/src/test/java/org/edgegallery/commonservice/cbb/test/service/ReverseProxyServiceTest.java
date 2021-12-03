f807692ed909cedd91c0e2ee08e0883503375bba
cuijianwei <cjwtyllr@163.com>
2021-12-03 17:22:10 +0800

    单元测试&删掉部分无用代码

--- common-service-cbb/src/test/java/org/edgegallery/commonservice/cbb/test/service/ReverseProxyServiceTest.java
+++ common-service-cbb/src/test/java/org/edgegallery/commonservice/cbb/test/service/ReverseProxyServiceTest.java
@@ -1,9 +1,11 @@
 package org.edgegallery.commonservice.cbb.test.service;
 
-import com.google.gson.Gson;
+import mockit.Mock;
+import mockit.MockUp;
 import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.io.FileUtils;
+import org.edgegallery.commonservice.cbb.exception.CommonServiceCbException;
 import org.edgegallery.commonservice.cbb.model.ReverseProxy;
-import org.edgegallery.commonservice.cbb.service.ReverseProxyService;
 import org.edgegallery.commonservice.cbb.service.impl.ReverseProxyServiceImpl;
 import org.edgegallery.commonservice.cbb.test.CommonServiceCbbTests;
 import org.junit.Assert;
@@ -15,54 +17,94 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.client.RestTemplate;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = CommonServiceCbbTests.class)
 @AutoConfigureMockMvc
-public class ReverseProxyServiceTest {
+public class ReverseProxyServiceTest{
     @Autowired
     private ReverseProxyServiceImpl reverseProxyService;
 
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
+        Class.forName("mockit.integration.junit4.JMockit");
+        new MockUp<FileUtils>() {
+            @Mock
+            public void writeStringToFile(final File file, final String data, final Charset encoding)
+                    throws IOException {
+
+            }
+        };
+
+        DefaultExecutor executor = Mockito.mock(DefaultExecutor.class);
+        Mockito.when(executor.execute(Mockito.any())).thenReturn(0);
+        reverseProxyService.setExecutor(executor);
+
+        RestTemplate restTemplate = Mockito.mock(RestTemplate.class);
+        String body = "{\"destHostIp\":\"192.168.1.101\",\"destHostPort\":6080,\"localPort\":30111," +
+                "\"nextHopProtocol\":\"http\",\"nextHopIp\":\"192.168.1.2\",\"nextHopPort\":30111," +
+                "\"linkNumber\":1,\"hopIndex\":1}";
+        ResponseEntity<String> response = new ResponseEntity<>(body, HttpStatus.OK);
+        Mockito.when((restTemplate.exchange(Mockito.anyString(), Mockito.any(),
+                new HttpEntity<>(Mockito.any(), Mockito.any()), String.class))).thenReturn(response);
+        reverseProxyService.setRestTemplate(restTemplate);
     }
 
     @Test
     public void testAddReverseProxySuccess1() throws Exception {
-        DefaultExecutor executor = Mockito.mock(DefaultExecutor.class);
-        Mockito.when(executor.execute(Mockito.any())).thenReturn(0);
         ReverseProxy reverseProxy = new ReverseProxy("192.168.1.100", 6080, 0,
                 "http", null, 0,0,1);
-        reverseProxyService.setExecutor(executor);
-        reverseProxyService.addReverseProxy(reverseProxy, Mockito.anyString());
+        ReverseProxy result = reverseProxyService.addReverseProxy(reverseProxy, "token");
+        Assert.assertEquals(result.getLocalPort(), 30111);
+        reverseProxy = new ReverseProxy("192.168.1.102", 6080, 0,
+                "http", null, 0,0,1);
+        result = reverseProxyService.addReverseProxy(reverseProxy, "token");
+        Assert.assertEquals(result.getLocalPort(), 30113);
+        reverseProxy = new ReverseProxy("192.168.1.103", 6080, 0,
+                "http", "192.168.1.110", 6080,0,2);
+        result = reverseProxyService.addReverseProxy(reverseProxy, "token");
+        Assert.assertEquals(result.getLocalPort(), 30114);
+        reverseProxyService.addReverseProxy(reverseProxy, "token");
+        reverseProxyService.deleteReverseProxy("192.168.1.103", 6080, "token");
+
+        reverseProxy = new ReverseProxy("192.168.1.105", 6080, 0,
+                "http", "192.168.1.110", 6080,0,1);
+        try {
+            reverseProxyService.addReverseProxy(reverseProxy, "token");
+        }catch (Exception e) {
+            Assert.assertEquals(e.getClass(), CommonServiceCbException.class);
+        }
     }
 
     @Test
-    public void testAddReverseProxySuccess2() throws Exception {
-        DefaultExecutor executor = Mockito.mock(DefaultExecutor.class);
-        Mockito.when(executor.execute(Mockito.any())).thenReturn(0);
-        ReverseProxy reverseProxy = new ReverseProxy("192.168.1.100", 6080, 0,
-                "http", null, 0,0,1);
-        reverseProxyService.setExecutor(executor);
-        RestTemplate restTemplate = Mockito.mock(RestTemplate.class);
-        ResponseEntity<String> response = new ResponseEntity<>(HttpStatus.OK);
-        Mockito.when((restTemplate.exchange(Mockito.anyString(), Mockito.any(),
-                new HttpEntity<>(Mockito.any(), Mockito.any()), String.class))).thenReturn(response);
-        reverseProxyService.setRestTemplate(restTemplate);
-        reverseProxyService.addReverseProxy(reverseProxy, Mockito.anyString());
+    public void testDeleteReverseProxySuccess1() throws Exception {
+        reverseProxyService.deleteReverseProxy("192.168.1.101", 6080, "token");
+        reverseProxyService.deleteReverseProxy("192.168.1.101", 6080, "token");
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testDeleteReverseProxySuccess2() throws Exception {
+        reverseProxyService.deleteReverseProxy("192.168.1.100", 6080, "token");
+        reverseProxyService.deleteReverseProxy("192.168.1.100", 6080, "token");
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testGetReverseProxy() throws Exception {
+        ReverseProxy result = reverseProxyService.getReverseProxy("192.168.1.103", 6080);
+        Assert.assertEquals(result.getDestHostIp(), "192.168.1.103");
+        result = reverseProxyService.getReverseProxy("192.168.1.200", 6080);
+        Assert.assertNull(result);
     }
 }
--- common-service-cbb/src/test/resources/mec-data.sql
+++ common-service-cbb/src/test/resources/mec-data.sql
@@ -17,3 +17,9 @@
 -- ----------------------------
 -- Records of tbl_service_host
 -- ----------------------------
+insert into tbl_reverse_proxy (dest_host_ip, dest_host_port, local_port, next_hop_protocol,
+                                   next_hop_ip, next_hop_port, link_number, hop_index)
+    values ('192.168.1.100', 6080, 30111, 'http','192.168.1.101', 30111, 1, 1);
+insert into tbl_reverse_proxy (dest_host_ip, dest_host_port, local_port, next_hop_protocol,
+                                   next_hop_ip, next_hop_port, link_number, hop_index)
+    values ('192.168.1.101', 6080, 30112, '','', 0, 1, 2);
